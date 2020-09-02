4778f3fac97e875bcf5cfdfcf4e99ad127aacf6e
Vadim <vadim@kravciuk.com>
2020-09-02 12:03:24 +0300

    Fixes

--- vu/templatetags/vu_tags.py
+++ vu/templatetags/vu_tags.py
@@ -13,10 +13,13 @@ register = template.Library()
 
 @register.filter()
 def multiply(source, amount):
+    if source is None:
+        return 0
     try:
         return Math.round(Decimal(source)*Decimal(amount))
     except Exception as e:
         log.error(e, exc_info=True)
+        return 0
 
 
 @register.filter()
